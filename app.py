8ce42bfe93543e63ef020058dee73a7fe8af5bb1
Prajwal Hiremath <prajwalhiremath2017@gmail.com>
2023-11-11 20:31:01 +0530

    web app is ready

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template, redirect, flash
+from flask import Flask, request, app, jsonify, url_for, render_template, redirect
 import numpy as np
 import pandas as pd
 
@@ -23,6 +23,14 @@ def predict_api():
     print(output[0])#this (output) will be a 2D array, so we are taking 1st one
     return jsonify(output[0])
 
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text="The house price is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
